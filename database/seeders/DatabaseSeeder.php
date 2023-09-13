43cbae830fef522852549845bd2cd58b3c3adda9
Marvin M. Ramos <115031416+Crispkreme@users.noreply.github.com>
2023-09-12 23:37:44 -0700

    added migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         $this->call(RoleSeeder::class);
+        $this->call(StatusSeeder::class);
         $this->call(GenderSeeder::class);
     }
 }
