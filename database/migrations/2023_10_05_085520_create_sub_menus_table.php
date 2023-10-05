96d4a4bc696178513092c58b8ed6e76ae261bf61
Criskpreme <marvinramos.nutnull@gmail.com>
2023-10-05 17:20:30 +0800

    menu
    - submenu pages

---
+++ database/migrations/2023_10_05_085520_create_sub_menus_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sub_menus', function (Blueprint $table) {
+            $table->id();
+            $table->string('list');
+            $table->string('url');
+            $table->string('icon');
+            $table->string('class');
+            $table->boolean('isActive');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sub_menus');
+    }
+};
