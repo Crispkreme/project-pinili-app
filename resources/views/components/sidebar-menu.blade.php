152fca2539e55e6d95770ece5b594f96464e4434
Criskpreme <marvinramos.nutnull@gmail.com>
2023-10-12 08:11:50 +0800

    Update sidebar-menu.blade.php
    - fix error

--- resources/views/components/sidebar-menu.blade.php
+++ resources/views/components/sidebar-menu.blade.php
@@ -6,18 +6,19 @@
                     <span class="{{ $sidebarMenuList['class'] }}">{{ $sidebarMenuList['list'] ?? '' }}</span>
                 @else
                     @if(isset($sidebarMenuList['list']))
-                        @if(isset($sidebarMenuList['url']))
+                        @if(isset($sidebarMenuList['submenu']) && count($sidebarMenuList['submenu']) > 0)
+                            <a href="#" data-toggle="collapse" data-target="#submenu-{{ $loop->index }}" class="{{ $sidebarMenuList['class'] }}">
+                                {!! $sidebarMenuList['icon'] !!}
+                                <span>{{ $sidebarMenuList['list'] }}</span>
+                            </a>
+                            <ul id="submenu-{{ $loop->index }}" class="sub-menu collapse" aria-expanded="false">
+                                <x-sidebar-menu :sidebarMenuLists="$sidebarMenuList['submenu']" />
+                            </ul>
+                        @else
                             <a href="{{ $sidebarMenuList['url'] }}" class="{{ $sidebarMenuList['class'] }}">
                                 {!! $sidebarMenuList['icon'] !!}
                                 <span>{{ $sidebarMenuList['list'] }}</span>
                             </a>
-                        @else
-                            <span class="{{ $sidebarMenuList['class'] }}">{{ $sidebarMenuList['list'] }}</span>
-                        @endif
-                        @if(isset($sidebarMenuList['submenu']))
-                            <ul class="sub-menu" aria-expanded="false">
-                                <x-sidebar-menu :sidebarMenuLists="$sidebarMenuList['submenu']" />
-                            </ul>
                         @endif
                     @endif
                 @endif
