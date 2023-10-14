a76c69579dd9dc421458caf30dbaa11980de026a
Marvin M. Ramos <115031416+Crispkreme@users.noreply.github.com>
2023-10-14 13:31:59 -0700

    fixing the sidebar functionality

--- resources/views/components/sidebar-menu.blade.php
+++ resources/views/components/sidebar-menu.blade.php
@@ -1,17 +1,17 @@
 <div>
     <ul class="metismenu list-unstyled" id="side-menu">
-        @foreach($sidebarMenuLists as $sidebarMenuList)
-            <li>
+        @foreach($sidebarMenuLists as $index => $sidebarMenuList)
+            <li id="side-main-menu-{{ $index }}" data-index="{{ $index }}">
                 @if(isset($sidebarMenuList['isMenuTitle']) && $sidebarMenuList['isMenuTitle'])
                     <span class="{{ $sidebarMenuList['class'] }}">{{ $sidebarMenuList['list'] ?? '' }}</span>
                 @else
                     @if(isset($sidebarMenuList['list']))
                         @if(isset($sidebarMenuList['submenu']) && count($sidebarMenuList['submenu']) > 0)
-                            <a href="#" data-toggle="collapse" data-target="#submenu-{{ $loop->index }}" class="{{ $sidebarMenuList['class'] }}">
+                            <a href="#" data-toggle="collapse" data-target="#submenu-{{ $loop->index }}" class="{{ $sidebarMenuList['class'] }}" aria-expanded="false">
                                 {!! $sidebarMenuList['icon'] !!}
                                 <span>{{ $sidebarMenuList['list'] }}</span>
                             </a>
-                            <ul id="submenu-{{ $loop->index }}" class="sub-menu collapse" aria-expanded="false">
+                            <ul id="submenu-{{ $loop->index }}" class="sub-menu collapse mm-collapse" aria-expanded="false" style="">
                                 <x-sidebar-menu :sidebarMenuLists="$sidebarMenuList['submenu']" />
                             </ul>
                         @else
@@ -26,3 +26,27 @@
         @endforeach
     </ul>
 </div>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        const menuItems = document.querySelectorAll('[data-index]');
+
+        menuItems.forEach(function(item) {
+            item.addEventListener('click', function() {
+                const index = item.getAttribute('data-index');
+                const subMenu = document.querySelector(`#submenu-${index}`);
+                
+                // Toggle the "aria-expanded" attribute
+                const isExpanded = subMenu.getAttribute('aria-expanded') === 'true';
+                subMenu.setAttribute('aria-expanded', isExpanded ? 'false' : 'true');
+
+                // Toggle the "mm-show" class
+                if (isExpanded) {
+                    subMenu.classList.remove('mm-show');
+                } else {
+                    subMenu.classList.add('mm-show');
+                }
+            });
+        });
+    });
+</script>
